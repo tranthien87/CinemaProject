f1433bdec8d53cd723367a8d7fcca30342eac75e
NguyenThien <tranthien8789@gmail.com>
2019-08-07 13:25:53 +0700

    create component pagenot found

--- src/app/entities/entities.module.ts
+++ src/app/entities/entities.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EntitiesComponent } from './entities.component';
 import { EntitiesRoutingModule } from './entities-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [EntitiesComponent],
+  declarations: [EntitiesComponent, PageNotFoundComponent],
   imports: [
     CommonModule, EntitiesRoutingModule
   ]
